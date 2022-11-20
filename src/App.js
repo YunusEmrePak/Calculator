a017f74332cb955678a71a596ee2f6a1f443106e
Yunus Emre Pak <yunusemrepak@windowslive.com>
2022-11-20 14:29:34 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Nav, Buttons } from "./Buttons";
 
+let conc = 0;
+let plusCount = 0, minusCount = 0, multiCount = 0, divideCount = 0, modCount = 0;
 function App() {
+  const [input, setInput] = useState("");
+
+  function changeInput(updatedInput, end = false, control = false, clear = false, plus = false, minus = false, multi = false, divide = false, mod = false) {
+    let text = input;
+    if (!end) {
+      text += updatedInput;
+      setInput(text);
+      if (control) {
+        conc += parseInt(text, 10);
+        text = '';
+        setInput('');
+        if (plus) {
+            plusCount += 1;
+        }
+        else if (minus) {
+            minusCount += 1;
+        }
+        else if (multi) {
+            multiCount += 1;
+        }
+        else if (divide) {
+            divideCount += 1;
+        }
+        else if (mod) {
+            modCount += 1;
+        }
+      }
+      if (clear) {
+        text = '';
+        setInput('');
+      }
+
+    
+    } 
+    else if (end){
+        if (plusCount != 0) {
+            conc += parseInt(text, 10);
+            plusCount = 0;
+        }
+        else if (minusCount != 0) {
+            conc -= parseInt(text, 10);
+            minusCount = 0;
+        }
+        else if (multiCount != 0) {
+            conc *= parseInt(text, 10);
+            multiCount = 0;
+        }
+        else if (divideCount != 0) {
+            conc /= parseInt(text, 10);
+            divideCount = 0;
+        }
+        else if (modCount != 0) {
+            conc %= parseInt(text, 10);
+            modCount = 0;
+        }
+        text = '';
+        setInput(conc);
+        conc = 0;
+        end = false;
+    }
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="back">
+      <div>
+        <Nav currentInput={input} />
+      </div>
+      <div className="row row1 clear">
+        {<Buttons value="<--" class="clearLast"/>}
+        {<Buttons value="%" class="mod" changeInput={changeInput} control={true} mod={true} />}
+        {<Buttons value="Clear" class="clearElements" changeInput={changeInput} clear={true}/>}
+      </div>
+      <div className="row row2">
+        {<Buttons value="7" class="number7" changeInput={changeInput} />}
+        {<Buttons value="8" class="number8" changeInput={changeInput} />}
+        {<Buttons value="9" class="number9" changeInput={changeInput} />}
+        {<Buttons value="/" class="divide" changeInput={changeInput} control={true} divide={true} />}
+      </div>
+      <div className="row row3">
+        {<Buttons value="4" class="number4" changeInput={changeInput} />}
+        {<Buttons value="5" class="number5" changeInput={changeInput} />}
+        {<Buttons value="6" class="number6" changeInput={changeInput} />}
+        {<Buttons value="*" class="multi" changeInput={changeInput} control={true} multi={true} />}
+      </div>
+      <div className="row row4">
+        {<Buttons value="1" class="number1" changeInput={changeInput} />}
+        {<Buttons value="2" class="number2" changeInput={changeInput} />}
+        {<Buttons value="3" class="number3" changeInput={changeInput} />}
+        {<Buttons value="-" class="minus" changeInput={changeInput} control={true} minus={true} />}
+      </div>
+      <div className="row row5">
+        {<Buttons value="0" class="number0" changeInput={changeInput} />}
+        {<Buttons value="." class="dot" changeInput={changeInput} />}
+        {<Buttons value="=" class="equal" changeInput={changeInput} ender={true}/>}
+        {<Buttons value="+" class="add" changeInput={changeInput} control={true} plus={true} />}
+      </div>
     </div>
   );
 }
---
+++ src/Buttons.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export function Nav({ currentInput }) {
+  return (
+    <div className="inpDiv">
+      <input className="calcInput" value={currentInput} />
+    </div>
+  );
+}
+
+export function Buttons(props) {
+  function Calculation() {
+    props.changeInput(props.value, props.ender, props.control, props.clear, props.plus, props.minus, props.multi, props.divide, props.mod);
+  }
+
+  return (
+    <button className={props.class} onClick={Calculation}>
+      {props.value}
+    </button>
+  );
+}
